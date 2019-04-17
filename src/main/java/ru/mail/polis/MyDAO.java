1cbce09c38c03d4074cf5f2ae112ec6fa7857154
Vladislava Vasileva <vlada318@live.com>
2019-04-17 22:44:37 +0300

    some changes

--- src/main/java/ru/mail/polis/MyDAO.java
+++ src/main/java/ru/mail/polis/MyDAO.java
@@ -2,7 +2,6 @@ package ru.mail.polis;
 
 import org.jetbrains.annotations.NotNull;
 
-import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.util.Iterator;
 import java.util.NavigableMap;
@@ -13,22 +12,23 @@ public class MyDAO implements DAO {
 
   @NotNull
   @Override
-  public Iterator<Record> iterator(@NotNull ByteBuffer from){
+  public Iterator<Record> iterator(@NotNull final ByteBuffer from){
     return database.tailMap(from).values().iterator();
   }
 
   @Override
-  public void upsert(@NotNull ByteBuffer key, @NotNull ByteBuffer value){
+  public void upsert(@NotNull final ByteBuffer key, @NotNull final ByteBuffer value){
     database.put(key, Record.of(key, value));
   }
 
   @Override
-  public void remove(@NotNull ByteBuffer key){
+  public void remove(@NotNull final ByteBuffer key){
     database.remove(key);
   }
 
   @Override
   public void close(){
-
+    //comes soon
+    throw new UnsupportedOperationException();
   }
 }
